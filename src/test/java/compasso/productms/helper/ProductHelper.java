922fcd55e0d1db794e022a2cfb0f83d5ec90317f
daniel.zat <daniel.zat@cwi.com.br>
2021-11-21 18:36:51 -0300

    Adicionado testes para classe ProductMapper

--- src/test/java/compasso/productms/helper/ProductHelper.java
+++ src/test/java/compasso/productms/helper/ProductHelper.java
@@ -1,5 +1,20 @@
 package compasso.productms.helper;
 
+import compasso.productms.entity.ProductEntity;
+import compasso.productms.request.ProductRequest;
+import compasso.productms.response.ProductResponse;
+
 public class ProductHelper {
 
+    public static ProductRequest createProductRequest() {
+        return ProductRequest.builder().name("Arroz").description("Arroz Tio João").price(7.50).build();
+    }
+
+    public static ProductResponse createProductResponse() {
+        return ProductResponse.builder().id("6199a4ba8cdb5f7e50643648").name("Feijão").description("Feijão namorado").price(7.50).build();
+    }
+
+    public static ProductEntity createProductEntity() {
+        return ProductEntity.builder().id("6199a4ba8cdb5f7e50643648").name("Feijão").description("Feijão namorado").price(7.50).build();
+    }
 }
--- src/test/java/compasso/productms/mapper/ProductMapperTest.java
+++ src/test/java/compasso/productms/mapper/ProductMapperTest.java
@@ -1,21 +1,54 @@
 package compasso.productms.mapper;
 
+import compasso.productms.entity.ProductEntity;
+import compasso.productms.helper.ProductHelper;
+import compasso.productms.request.ProductRequest;
+import compasso.productms.response.ProductResponse;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 
 public class ProductMapperTest {
 
     @Test
     public void shouldConvertRequestToEntitySuccessfully() {
 
+        ProductRequest productRequest = ProductHelper.createProductRequest();
+
+        ProductEntity productEntity =  ProductMapper.convertRequestToEntity(productRequest);
+
+        Assertions.assertNotNull(productEntity);
+        Assertions.assertEquals(productRequest.getName(), productEntity.getName());
+        Assertions.assertEquals(productRequest.getDescription(), productEntity.getDescription());
+        Assertions.assertEquals(productRequest.getPrice(), productEntity.getPrice());
     }
 
     @Test
     public void shouldConvertEntityToResponseSuccessfully() {
 
+        ProductEntity productEntity =  ProductHelper.createProductEntity();
+
+        ProductResponse productResponse = ProductMapper.convertEntityToResponse(productEntity);
+
+        Assertions.assertNotNull(productResponse);
+        Assertions.assertEquals(productEntity.getId(), productResponse.getId());
+        Assertions.assertEquals(productEntity.getName(), productResponse.getName());
+        Assertions.assertEquals(productEntity.getDescription(), productResponse.getDescription());
+        Assertions.assertEquals(productEntity.getPrice(), productResponse.getPrice());
     }
 
     @Test
     public void shouldUpdateProductEntityWithProductRequestSuccessfully() {
 
+        ProductRequest productRequest =  ProductHelper.createProductRequest();
+
+        ProductEntity productEntity = ProductHelper.createProductEntity();
+
+        ProductEntity productEntityUpdated = ProductMapper.updateProductEntityWithProductRequest(productEntity, productRequest);
+
+        Assertions.assertNotNull(productEntityUpdated);
+        Assertions.assertEquals(productRequest.getName(), productEntity.getName());
+        Assertions.assertEquals(productRequest.getDescription(), productEntity.getDescription());
+        Assertions.assertEquals(productRequest.getPrice(), productEntity.getPrice());
     }
 }
